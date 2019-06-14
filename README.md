022049f1f5e3921fb466fac7e836c8149e3496f9
Bruno Camilo Grillo <bcgrillo@gfi.es>
2019-06-14 11:38:48 +0200

    Correcciones

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-Task to update a Release or Build pipeline variable. 
+Task to update a variable in a variable group (library).
+
+Requires 'Allow scripts to access the OAuth token' in agent job additional options and administrator role to 'Project Collection Build Service' in the variable group.
+
 
 (preview)
--- variableupdater/run-task.ps1
+++ variableupdater/run-task.ps1
@@ -1,28 +1,29 @@
 Trace-VstsEnteringInvocation $MyInvocation
 
+$VariableGroupId = Get-VstsInput -Name VariableGroupId -Require
 $VariableName = Get-VstsInput -Name VariableName -Require
 $NewValue = Get-VstsInput -Name NewValue -Require
 
 Write-Output "Variable name: $($VariableName)"
 Write-Output "New value: $($NewValue)"
 
-$defUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/build/definitions/$($env:SYSTEM_DEFINITIONID)?api-version=5.0"
+$variableGroupUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/distributedtask/variablegroups/$($VariableGroupId)?api-version=5.0-preview.1"
 $authHeader = @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"}
 
-Write-Host "Build definition URL: $($defUri)"
-$definition = Invoke-RestMethod -Uri $defUri -Headers $authHeader
-Write-Host "Pipeline = $($definition | ConvertTo-Json -Depth 100)"
+Write-Host "Variable group URI: $($variableGroupUri)"
+$definition = Invoke-RestMethod -Uri $variableGroupUri -Headers $authHeader
+Write-Host "Variable group = $($definition | ConvertTo-Json -Depth 100)"
 
-if ($definition.variables.$VariableName) {
+if ($definition -and $definition.variables.$VariableName) {
     Write-Host "Old value: $($definition.variables.$VariableName.Value)"
     
     $definition.variables.$VariableName.Value = "$($NewValue)"
 
     $definitionJson = $definition | ConvertTo-Json -Depth 100 -Compress
 
-    Write-Verbose "Updating build definition: $($defUri)"
-    Invoke-RestMethod -Method Put -Uri $defUri -Headers $authHeader -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes($definitionJson)) | Out-Null
+    Write-Verbose "Updating variable group: $($variableGroupUri)"
+    Invoke-RestMethod -Method Put -Uri $variableGroupUri -Headers $authHeader -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes($definitionJson)) | Out-Null
 }
 else {
-    Write-Error "The variable can not be found on the definition: $($VariableName)"
+    Write-Error "The variable group or variable name can not be found..."
 }
