314ae5769772650f8b281d17422f493aee77fdea
Bruno Camilo Grillo <bcgrillo@gfi.es>
2019-09-24 10:45:07 +0200

    Fix #2

--- variableupdater/run-task.ps1
+++ variableupdater/run-task.ps1
@@ -5,18 +5,18 @@ $VariableName = Get-VstsInput -Name VariableName -Require
 $NewValue = Get-VstsInput -Name NewValue -Require
 
 Write-Output "Variable name: $($VariableName)"
-Write-Output "New value: $($NewValue)"
 
-$variableGroupUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/distributedtask/variablegroups/$($VariableGroupId)?api-version=5.0-preview.1"
+$variableGroupUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/distributedtask/variablegroups/$($VariableGroupId)?api-version=4.1-preview.1"
 $authHeader = @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"}
 
-Write-Host "Variable group URI: $($variableGroupUri)"
+Write-Verbose "Variable group URI: $($variableGroupUri)"
 $definition = Invoke-RestMethod -Uri $variableGroupUri -Headers $authHeader
-Write-Host "Variable group = $($definition | ConvertTo-Json -Depth 100)"
+Write-Verbose "Variable group = $($definition | ConvertTo-Json -Depth 100)"
 
 if ($definition -and $definition.variables.$VariableName) {
-    Write-Host "Old value: $($definition.variables.$VariableName.Value)"
-    
+    Write-Output "Old value: $($definition.variables.$VariableName.Value)"
+    Write-Output "New value: $($NewValue)"
+
     $definition.variables.$VariableName.Value = "$($NewValue)"
 
     $definitionJson = $definition | ConvertTo-Json -Depth 100 -Compress
