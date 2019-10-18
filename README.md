cadc9fe9ee557dd2474e48399ee3ecfd80b74625
Bruno Camilo Grillo <bcgrillo@gfi.es>
2019-10-18 11:14:21 +0200

    Fix #3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Task to update a variable in a variable group (library).
 
-Requires 'Allow scripts to access the OAuth token' in agent job additional options and administrator role to 'Project Collection Build Service' in the variable group.
+Important: Requires 'Allow Scripts to Access OAuth Token' enabled in the agent job additional options and set administrator role to 'Project Collection Build Service' in the variable group.
 
 
 (preview)
--- variableupdater/run-task.ps1
+++ variableupdater/run-task.ps1
@@ -4,10 +4,16 @@ $VariableGroupId = Get-VstsInput -Name VariableGroupId -Require
 $VariableName = Get-VstsInput -Name VariableName -Require
 $NewValue = Get-VstsInput -Name NewValue -Require
 
+if (!(Get-VstsTaskVariable -Name "System.AccessToken")) {
+    throw ("OAuth token not found. Make sure to have 'Allow Scripts to Access OAuth Token' enabled in the agent job additional options.")
+}
+
+$AccessToken = Get-VstsTaskVariable -Name "System.AccessToken"
+
 Write-Output "Variable name: $($VariableName)"
 
 $variableGroupUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/distributedtask/variablegroups/$($VariableGroupId)?api-version=4.1-preview.1"
-$authHeader = @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"}
+$authHeader = @{Authorization = "Bearer $AccessToken"}
 
 Write-Verbose "Variable group URI: $($variableGroupUri)"
 $definition = Invoke-RestMethod -Uri $variableGroupUri -Headers $authHeader
@@ -25,5 +31,5 @@ if ($definition -and $definition.variables.$VariableName) {
     Invoke-RestMethod -Method Put -Uri $variableGroupUri -Headers $authHeader -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes($definitionJson)) | Out-Null
 }
 else {
-    Write-Error "The variable group or variable name can not be found..."
+    Write-Error "The variable group or variable name can not be found, or the 'Project Collection Build Service' doesn't have administrator role in the variable group."
 }
