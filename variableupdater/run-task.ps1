9da0e3b8c724693216fc429ecfc66e6abe24d7f6
Bruno Camilo Grillo <bcgrillo@gfi.es>
2019-10-23 17:46:53 +0200

    Fix #4

--- variableupdater/run-task.ps1
+++ variableupdater/run-task.ps1
@@ -3,6 +3,7 @@ Trace-VstsEnteringInvocation $MyInvocation
 $VariableGroupId = Get-VstsInput -Name VariableGroupId -Require
 $VariableName = Get-VstsInput -Name VariableName -Require
 $NewValue = Get-VstsInput -Name NewValue -Require
+$UseBasicParsing = Get-VstsInput -Name UseBasicParsing -AsBool
 
 if (!(Get-VstsTaskVariable -Name "System.AccessToken")) {
     throw ("OAuth token not found. Make sure to have 'Allow Scripts to Access OAuth Token' enabled in the agent job additional options.")
@@ -16,7 +17,14 @@ $variableGroupUri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMP
 $authHeader = @{Authorization = "Bearer $AccessToken"}
 
 Write-Verbose "Variable group URI: $($variableGroupUri)"
-$definition = Invoke-RestMethod -UseBasicParsing -Uri $variableGroupUri -Headers $authHeader
+
+if ($UseBasicParsing) {
+	$definition = Invoke-RestMethod -UseBasicParsing -Uri $variableGroupUri -Headers $authHeader
+}
+else {
+	$definition = Invoke-RestMethod -Uri $variableGroupUri -Headers $authHeader
+}
+
 Write-Verbose "Variable group = $($definition | ConvertTo-Json -Depth 100)"
 
 if ($definition -and $definition.variables.$VariableName) {
@@ -28,7 +36,13 @@ if ($definition -and $definition.variables.$VariableName) {
     $definitionJson = $definition | ConvertTo-Json -Depth 100 -Compress
 
     Write-Verbose "Updating variable group: $($variableGroupUri)"
-    Invoke-RestMethod -UseBasicParsing -Method Put -Uri $variableGroupUri -Headers $authHeader -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes($definitionJson)) | Out-Null
+    
+	if ($UseBasicParsing) {
+		Invoke-RestMethod -UseBasicParsing -Method Put -Uri $variableGroupUri -Headers $authHeader -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes($definitionJson)) | Out-Null
+	}
+	else {
+		Invoke-RestMethod -Method Put -Uri $variableGroupUri -Headers $authHeader -ContentType "application/json" -Body ([System.Text.Encoding]::UTF8.GetBytes($definitionJson)) | Out-Null
+	}
 }
 else {
     Write-Error "The variable group or variable name can not be found, or the 'Project Collection Build Service' doesn't have administrator role in the variable group."
